58ab914c39822dd52a39c549197a23c3fac85b0b
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2023-06-11 05:38:23 +0100

    added this view sql file and products. This is because of the fact that sql views doesnt allow me to check for existense

--- Golang-Store-Backend/sql/Products.sql
+++ Golang-Store-Backend/sql/Products.sql
@@ -122,4 +122,4 @@ CREATE TABLE IF NOT EXISTS tblAttribute (
   FOREIGN KEY (Variation_ID) REFERENCES tblVariation (Variation_ID) ON DELETE CASCADE
 );
 
-CREATE VIEW PrimeSubFinalCategoryProducts AS SELECT tblProducts.Product_ID, tblProducts.Product_Name, tblCategoriesPrime.CategoryName FROM tblProducts JOIN tblCatFinalProd ON tblCatFinalProd.Product_ID = tblProducts.Product_ID JOIN tblCategoriesFinal ON tblCategoriesFinal.Category_ID = tblCatFinalProd.CatFinalID JOIN tblCatSubFinal ON tblCatSubFinal.CatFinalID = tblCategoriesFinal.Category_ID JOIN tblCategoriesSub ON tblCategoriesSub.Category_ID = tblCatSubFinal.CatSubID JOIN tblCatPrimeSub ON tblCatPrimeSub.CatSubID = tblCategoriesSub.Category_ID JOIN tblCategoriesPrime ON tblCategoriesPrime.Category_ID = tblCatPrimeSub.CatPrimeID ;
+
