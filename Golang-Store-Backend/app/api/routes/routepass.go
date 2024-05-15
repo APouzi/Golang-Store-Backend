1d8c47ab3373533df467e3340a99341f77da35d1
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-05-14 23:43:08 -0700

    Added cors and headers for settings. This will be refactored later -AP

--- Golang-Store-Backend/app/api/routes/routepass.go
+++ Golang-Store-Backend/app/api/routes/routepass.go
@@ -10,6 +10,7 @@ import (
 	testroutes "github.com/Apouzi/golang-shop/app/api/routes/test-routes"
 	userendpoints "github.com/Apouzi/golang-shop/app/api/routes/user"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -28,6 +29,33 @@ func RouteDigest(digest *chi.Mux, db *sql.DB, redis *redis.Client) *chi.Mux{
 	AuthMiddleWare := authorization.InjectDBRef(db)
 
 	rTestRoutes := testroutes.InjectDBRef(db, redis)
+
+	c := cors.New(cors.Options{
+        // AllowedOrigins is a list of origins a cross-domain request can be executed from
+        // All origins are allowed by default, you don't need to set this.
+        AllowedOrigins: []string{"http://localhost:4200"},
+        // AllowOriginFunc is a custom function to validate the origin. It takes the origin
+        // as an argument and returns true if allowed or false otherwise. 
+        // If AllowOriginFunc is set, AllowedOrigins is ignored.
+        // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+
+        // AllowedMethods is a list of methods the client is allowed to use with
+        // cross-domain requests. Default is all methods.
+        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+
+        // AllowedHeaders is a list of non simple headers the client is allowed to use with
+        // cross-domain requests.
+        AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+
+        // ExposedHeaders indicates which headers are safe to expose to the API of a CORS
+        // API specification
+        ExposedHeaders:   []string{"Link"},
+        AllowCredentials: true,
+        // MaxAge indicates how long (in seconds) the results of a preflight request
+        // can be cached
+        MaxAge: 300, // 5 minutes
+    })
+	digest.Use(c.Handler)
 
 	//Index
 	digest.Get("/", rIndex.Index)
